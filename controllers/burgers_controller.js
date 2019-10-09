c101b4ac5407c0e271eb571af35f44ac8ec39ed7
tylermorrisford <tylerford@gmail.com>
2019-10-08 22:21:39 -0600

    build MVC file structure

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var burger = require('./burger.js');
+
+
+
+// routes
---
+++ models/burger.js
@@ -0,0 +1,11 @@
+const orm = require("../config/orm");
+
+// create the code that will call the ORM functions using donut specific input for the ORM
+
+
+// form allows user to add a donut with button to devour
+// donut (not yet devoured) displays with id and button
+// clicking the button adds donut to devoured
+
+
+module.exports = burger;
